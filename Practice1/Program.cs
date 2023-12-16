ce5efaa6476caba44266232830a7eff33efd1d1c
ckmeleth <murad.savelev@inbox.ru>
2023-12-16 10:17:12 +0300

    Добавьте файлы проекта.

---
+++ Practice1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!!!");
+            Console.ReadKey();
+
+
+            Console.Write("Hello, ");
+            Console.Write("world");
+            Console.Write("!!!");
+            Console.ReadLine();
+        }
+    }
+}
